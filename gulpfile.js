2726d18b41657bb81a599c2478bdbbc8fcd089d8
nickzuber <nickzoober@gmail.com>
2016-02-18 01:18:30 -0500

    building files and refactoring code

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,7 @@ var uglify = require('gulp-uglify'); // minify build files
 var rename = require('gulp-rename'); // custom name for build files
 var webpack = require('gulp-webpack'); // compiles React modules
 var header = require('gulp-header'); // custom comment header on build files
-
-var browserify = require('gulp-browserify'); // compiles React modules
+var react = require('gulp-react'); // transpiles jsx -> js
 
 // Set banner for production file
 var pkg = require('./package.json');
@@ -20,28 +19,24 @@ var banner = ['/*!',
 // Contacat & compress javascript files for module production
 gulp.task('build', function(){
 
-/*
-  gulp.src(['src/*.js'])
-  .pipe(browserify({
-    debug: true,
-    transform: ['reactify']
-  }))
+  gulp.src(['src/*.{js,jsx}'])
+  .pipe(react({harmony: true}))
   .pipe(uglify())
   .pipe(rename({
     basename: 'react-api',
     extname: '.min.js'
   }))
   .pipe(header(banner, {pkg: pkg}))
-  .pipe(gulp.dest('bin'));
-*/
-
-  // Move files to public directory for testing
-  gulp.src(['src/*.{js,jsx}'])
-  .pipe(gulp.dest('tests/modules/'));
+  .pipe(gulp.dest('build'));
 });
 
 // Contacat & compress javascript files for testing server
 gulp.task('test', function(){
+
+  // Move files to public directory for testing
+  gulp.src(['src/*.{js,jsx}'])
+  .pipe(gulp.dest('tests/modules/'));
+
   gulp.src(['tests/modules/render.js'])
   .pipe(webpack({
     watch: false,
--- src/react-api.jsx
+++ src/react-api.jsx
@@ -2,10 +2,11 @@
 'use strict';
 
 const React = require('react');
-//const request = require('./request.js');
-const md5 = require('../tools/crypto.js');
-const Component = require('./component.jsx');
 
+/** @component
+ * @prop {string} url, where to get the data from
+ * @prop {function} callback, what to do once we have that data
+ */
 const react_api = React.createClass({
 
   getInitialState: function(){
@@ -14,25 +15,63 @@ const react_api = React.createClass({
     };
   },
 
-  dataReady: function(){
-    console.log(this.state.data);
+  componentDidMount: function(){
+    var request = undefined;
+    var target = this.props.url;
+
+    if(window.XMLHttpRequest){
+      request = new XMLHttpRequest();
+    }else{
+      request = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    request.open("GET", target, true);
+
+    request.onreadystatechange = function(){
+      if(request.readyState == XMLHttpRequest.DONE){
+        if(request.status == 200){
+          this.fetchData(request.responseText);
+        }
+        else{
+          throw new Error("AJAX load failed: invalid status returned: " + request.status);
+        }
+      }
+    }.bind(this);
+
+    try{
+      request.send();
+    }
+    catch(e){
+      console.warn("Error: Connection refused");
+    }
+  },
+
+  fetchData: function(rawJSON){
+    try{
+      var data = JSON.parse(rawJSON);
+    }
+    catch(e){
+      throw new Error("Error: AJAX response was failed to parse as JSON");
+    }
+    
+    this.prepareData(data);
   },
 
   prepareData: function(data){
     this.setState({
         data: data
       }, this.dataReady);
-    console.log('prepareData function has fired and api data has just been set');
+  },
+
+  dataReady: function(){
+    console.log(this.state.data); // TODO: remove this line
+    this.props.callback(this.state.data);
   },
 
   render: function(){
-    var token = md5(Math.random().toString());
     return(
-      <div>parent
-        <Component
-          token={token}
-          prepareData={this.prepareData}
-        />  
+      <div>
+        react-api
       </div>
     );
   }
